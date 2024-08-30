72093254c51d31c145f334c4e612718c96769307
Artur Calderon <86498045+artur-calderon@users.noreply.github.com>
2024-08-29 23:34:39 -0400

    Update FirebaseConfig.js

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp ,cert} from "firebase-admin/app";
 import {getFirestore} from "firebase-admin/firestore"
-import Credential from "./etc/secrets/Credentials.json" assert{type:'json'}
+import Credential from "./Credentials.json" assert{type:'json'}
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
